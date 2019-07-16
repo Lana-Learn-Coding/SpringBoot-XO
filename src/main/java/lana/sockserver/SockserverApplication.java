ffb61071667be75a3171b94d7ba72827a887aa56
sitdownrightnow2552@gmail.com <toilatoan283>
2019-07-16 15:12:54 +0700

    initial project

---
+++ src/main/java/lana/sockserver/SockserverApplication.java
@@ -0,0 +1,13 @@
+package lana.sockserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockserverApplication.class, args);
+    }
+
+}
