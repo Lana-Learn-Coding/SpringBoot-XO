59dbd77a729f9a041e0f77df5d8917e9e8b6a144
sitdownrightnow2552@gmail.com <toilatoan283>
2019-07-19 18:28:22 +0700

    validate on form object

--- src/main/java/lana/sockserver/user/model/UserForm.java
+++ src/main/java/lana/sockserver/user/model/UserForm.java
@@ -3,10 +3,22 @@ package lana.sockserver.user.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 public class UserForm {
-    private Integer id;
+    @Size(max = 20, min = 6)
+    @NotBlank
     private String name;
-    private String phone;
+
+    @Size(max = 20, min = 6)
+    @NotBlank
+    private String password;
+
+    @Email
+    @NotBlank
+    private String email;
 }
