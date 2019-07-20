d3e8aedab41971b9ce102be0be93b7fd2d53058a
sitdownrightnow2552@gmail.com <toilatoan283>
2019-07-20 16:14:40 +0700

    add existByName and findByName methods

--- src/main/java/lana/sockserver/user/UserRepo.java
+++ src/main/java/lana/sockserver/user/UserRepo.java
@@ -3,6 +3,11 @@ package lana.sockserver.user;
 import lana.sockserver.user.model.UserEntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
 
-public interface UserRepo extends PagingAndSortingRepository<UserEntity,Integer> {
+
+public interface UserRepo extends PagingAndSortingRepository<UserEntity, Integer> {
+    boolean existsByName(String username);
+
+    Optional<UserEntity> findByName(String username);
 }
