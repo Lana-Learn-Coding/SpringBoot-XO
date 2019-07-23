551cb2cd14db8a195a8c6a7f1e3696da70d4c4c6
sitdownrightnow2552@gmail.com <toilatoan283>
2019-07-23 15:47:07 +0700

    rename

--- src/main/java/lana/sockserver/user/role/RoleEntity.java
+++ src/main/java/lana/sockserver/user/role/RoleEntity.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 @Table(name = "role")
-public class Role {
+public class RoleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
