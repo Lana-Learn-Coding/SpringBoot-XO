1318b54c11c09828a1dc56d9b5fee9ff67c3750b
Lana <sitdownrightnow2552@gmail.com>
2019-07-24 18:23:23 +0700

    temporary disable spring security

--- src/main/java/lana/sockserver/SecurityConfig.java
+++ src/main/java/lana/sockserver/SecurityConfig.java
@@ -16,7 +16,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/login","sign-up","/home")
                 .permitAll();
     }
