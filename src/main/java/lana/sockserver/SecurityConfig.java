98f40ce1cd613c0d21eefc4e307a4d48c40b96da
sitdownrightnow2552@gmail.com <toilatoan283>
2019-07-25 18:42:59 +0700

    config

--- src/main/java/lana/sockserver/SecurityConfig.java
+++ src/main/java/lana/sockserver/SecurityConfig.java
@@ -1,7 +1,9 @@
 package lana.sockserver;
 
+import lana.sockserver.user.role.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,15 +12,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    public SecurityConfig(){
+    public SecurityConfig() {
 
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("12345").roles("USER")
+                .and()
+                .withUser("admin").password("123456").roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login","sign-up","/home")
-                .permitAll();
+                .antMatchers("/login", "sign-up").permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/home").hasRole(Role.USER.name())
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("name")
+                .passwordParameter("password")
+                .failureForwardUrl("/login?error=true")
+                .successForwardUrl("/home");
     }
 }
