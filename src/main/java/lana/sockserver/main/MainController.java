e9c479bd39f3dc985888030cf0890945ee7c3750
sitdownrightnow2552@gmail.com <toilatoan283>
2019-07-26 18:33:31 +0700

    return home

--- src/main/java/lana/sockserver/main/MainController.java
+++ src/main/java/lana/sockserver/main/MainController.java
@@ -1,14 +1,17 @@
 package lana.sockserver.main;
 
-import lana.sockserver.user.User;
-import lana.sockserver.user.UserNotExistException;
+
 import lana.sockserver.user.UserService;
 import lana.sockserver.util.objectmapper.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.security.Principal;
+
 
 @Controller
 public class MainController {
@@ -21,16 +24,10 @@ public class MainController {
         this.userService = userService;
     }
 
-    @ModelAttribute
-    public UserDTO user(Principal principal) {
-        try {
-            User user = userService.get(principal.getName());
-            return objectMapper.map(user, UserDTO.class);
-        } catch (UserNotExistException e) {
-            // user must exist so it is in the principal :<
-            // but if this happen, return empty user
-            return new UserDTO();
-        }
+    @RequestMapping(value = "/home", method = {RequestMethod.GET, RequestMethod.POST})
+    public String home(Principal principal, Authentication auth, Model model) {
+        return "home";
     }
+
 
 }
