4c8818e1eec3ad339df62ab2772f37e278570e5c
lana <sitdownrightnow2552@gmail.com>
2019-07-27 00:28:01 +0700

    refactor

--- src/main/java/lana/sockserver/SecurityConfig.java
+++ src/main/java/lana/sockserver/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login", "sign-up").permitAll()
+                .antMatchers("/login", "/sign-up").permitAll()
                 .and()
                 .authorizeRequests()
                 .antMatchers("/home").hasAuthority(Role.USER.name())
