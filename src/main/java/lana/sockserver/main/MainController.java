26a6e2498385c9a42ecc1d9144330a45b3089acf
sitdownrightnow2552@gmail.com <toilatoan283>
2019-07-27 18:19:50 +0700

    handle user info
    handle user info and map into DTO class

--- src/main/java/lana/sockserver/main/MainController.java
+++ src/main/java/lana/sockserver/main/MainController.java
@@ -1,31 +1,37 @@
 package lana.sockserver.main;
 
 
-import lana.sockserver.user.service.UserService;
+import lana.sockserver.security.AuthenticationFacade;
+import lana.sockserver.user.controller.UserDTO;
+import lana.sockserver.user.service.UserNotExistException;
 import lana.sockserver.util.objectmapper.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.security.Principal;
-
 
 @Controller
 public class MainController {
+    private final AuthenticationFacade authenticationFacade;
     private final ObjectMapper objectMapper;
-    private final UserService userService;
 
     @Autowired
-    public MainController(ObjectMapper objectMapper, UserService userService) {
+    public MainController(ObjectMapper objectMapper, AuthenticationFacade authenticationFacade) {
         this.objectMapper = objectMapper;
-        this.userService = userService;
+        this.authenticationFacade = authenticationFacade;
     }
 
     @RequestMapping(value = "/home", method = {RequestMethod.GET, RequestMethod.POST})
-    public String home(Principal principal, Authentication authentication, Model model) {
-        return "home";
+    public String home(Model model) {
+        UserDTO userDTO = new UserDTO();
+        try {
+            objectMapper.map(authenticationFacade.getAuthenticatedUser(), userDTO);
+            model.addAttribute("user", userDTO);
+            return "home";
+        } catch (UserNotExistException e) {
+            return "home";
+        }
     }
 }
