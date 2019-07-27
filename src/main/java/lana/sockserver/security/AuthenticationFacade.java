81d6058245c413e8eefb44fa63db4847b2db4098
sitdownrightnow2552@gmail.com <toilatoan283>
2019-07-27 17:39:55 +0700

    get user
    get user directly instead of authentication

--- src/main/java/lana/sockserver/security/AuthenticationFacade.java
+++ src/main/java/lana/sockserver/security/AuthenticationFacade.java
@@ -1,7 +1,11 @@
 package lana.sockserver.security;
 
+import lana.sockserver.user.User;
+import lana.sockserver.user.service.UserNotExistException;
 import org.springframework.security.core.Authentication;
 
 public interface AuthenticationFacade {
     Authentication getAuthentication();
+
+    User getAuthenticatedUser() throws UserNotExistException;
 }
--- src/main/java/lana/sockserver/security/AuthenticationFacadeImpl.java
+++ src/main/java/lana/sockserver/security/AuthenticationFacadeImpl.java
@@ -1,12 +1,16 @@
 package lana.sockserver.security;
 
+import lana.sockserver.user.User;
+import lana.sockserver.user.UserRepo;
+import lana.sockserver.user.service.UserNotExistException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthenticationFacadeImpl implements AuthenticationFacade {
-
+    private UserRepo userRepo;
     private static AuthenticationFacadeImpl ourInstance = new AuthenticationFacadeImpl();
 
     private AuthenticationFacadeImpl() {
@@ -14,6 +18,19 @@ public class AuthenticationFacadeImpl implements AuthenticationFacade {
 
     public static AuthenticationFacadeImpl getInstance() {
         return ourInstance;
+    }
+
+    @Autowired
+    public void setUserRepo(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public User getAuthenticatedUser() throws UserNotExistException {
+        String username = getAuthentication().getName();
+        return userRepo
+                .findByName(username)
+                .orElseThrow(UserNotExistException::new);
     }
 
     @Override
